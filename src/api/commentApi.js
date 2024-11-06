718710173ad1157317db0cee6d4e8c0eec42df94
kde0707 <gammaeom1@gmail.com>
2024-11-06 15:33:42 +0900

    feat(comment): 댓글 리스트 호출 추가
    Front
     - CommentComponent.jsx
     - commentApi.js

--- src/api/commentApi.js
+++ src/api/commentApi.js
@@ -5,3 +5,8 @@ export const postComment = async (params) => {
     console.log(params)
     return (await axios.post(`${COMMENT}`, params)).data
 }
+
+export const getList = async (boardId) => {
+    console.log("댓글 리스트 불러오기" + boardId)
+    return (await axios.get(`${COMMENT}/${boardId}`)).data
+}
