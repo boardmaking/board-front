92732237dee1acfeba8d0a7aeaf89a9ba650971a
kde0707 <gammaeom1@gmail.com>
2024-11-12 17:07:20 +0900

    refactor(board): 게시글 작성 필요없는 줄 제거
    Front
     - WriteComponent.jsx

--- src/components/board/WriteComponent.jsx
+++ src/components/board/WriteComponent.jsx
@@ -183,8 +183,6 @@ const WriteComponent = () => {
         return newContent;
     };
 
-    const content = replaceBase64WithImageInfo(values);
-
   const modules = useMemo(() => {
     return {
       toolbar: {
