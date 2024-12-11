915c776ac735850edd34ba228de906c38c51af81
heosangbeom <luv_3_u@naver.com>
2024-12-12 08:41:11 +0900

    feat(main): 메인페이지 수정
    - 메인 페이지 수정
      - 메인 페이지 로그인 필요하도록 수정

--- src/components/MainIndexComponent.jsx
+++ src/components/MainIndexComponent.jsx
@@ -1,15 +1,22 @@
 import TableComponent from "./common/TableComponent.jsx";
 import BasicLayout from "../layouts/BasicLayout.jsx";
+import useCustomLogin from "../hooks/useCustomLogin.jsx";
 
 function MainIndexComponent() {
 
-    return (
-        <div>
-            <BasicLayout>
-                <TableComponent/>
-            </BasicLayout>
-        </div>
-    );
+  const {isLogin, moveToLoginReturn} = useCustomLogin()
+
+  if (!isLogin) {
+    return moveToLoginReturn()
+  }
+
+  return (
+      <div>
+        <BasicLayout>
+          <TableComponent/>
+        </BasicLayout>
+      </div>
+  );
 }
 
 export default MainIndexComponent;
